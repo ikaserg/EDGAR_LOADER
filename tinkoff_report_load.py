53e1bbed33136aec9ce71b85bc7db9c98e9e83f7
Sergey Karpukhin <ikaserg@gmail.com>
2020-07-15 22:50:44 +0300

    load tinkoff reports

---
+++ tinkoff_report_load.py
@@ -0,0 +1,23 @@
+import tinkoff
+import edgar_load
+import pandas as pd
+import os
+import datetime
+import time
+
+def load_all_tinkoff_reports(skip_path, token):
+    tin = tinkoff.load_ticker_dataframe(token)
+    sk = pd.read_csv(os.path.join(skip_path, 'skip.txt'), header=None)
+    for i, t in tin[(tin['type'] == 'Stock')&(tin['currency'] == 'USD')].iterrows():
+        if (t['ticker'] not in sk[0].values):
+            try:
+                print(t['ticker'], end = ': ')
+                last_time = datetime.datetime.now()
+                download_ticker_reports(t['ticker'], skip_path)        
+                load_time = datetime.datetime.now() - last_time
+                delay_step = 1
+                print(load_time)
+            except:            
+                print(' sleep: ' + str(load_time.total_seconds() * delay_step))
+                time.sleep(load_time.total_seconds() * delay_step)
+                delay_step = delay_step + 2
