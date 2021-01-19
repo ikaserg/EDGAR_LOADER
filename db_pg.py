fa58fe92e2a754736ff3412c78e5f030702749f3
Sergey Karpukhin <ikaserg@gmail.com>
2021-01-19 21:52:03 +0300

    db connect from ini file

--- db_pg.py
+++ db_pg.py
@@ -1,6 +1,15 @@
 import psycopg2
+import configparser
 
-def execute_query(p_db, p_sql, p_params):
+def get_db_connect(p_filename):
+	v_cfg = configparser.ConfigParser()
+	v_cfg.read(p_filename)
+	v_str = "host={host} dbname={dbname} user={user} password={pwd} port={port}"
+	return psycopg2.connect(v_str.format(host=v_cfg['connect']['host'], dbname=v_cfg['connect']['dbname'],\
+		       user=v_cfg['connect']['user'], pwd=v_cfg['connect']['password'], port=v_cfg['connect']['port']  ))
+
+
+def execute_query(p_db, p_sql, p_params = None):
 	v_cur = p_db.cursor()
 	if p_params is None:
 		v_cur.execute(p_sql) 	
@@ -8,13 +17,13 @@ def execute_query(p_db, p_sql, p_params):
 		v_cur.execute(p_sql, p_params)
 	return v_cur
 
-def do_query_one(p_db, p_sql, p_params):
+def do_query_one(p_db, p_sql, p_params = None):
 	v_cur = execute_query(p_db, p_sql, p_params)
 	result = v_cur.fetchone()
 	v_cur.close()
 	return result
 
-def do_exec_query(p_db, p_sql, p_params):
+def do_exec_query(p_db, p_sql, p_params = None):
 	v_cur = execute_query(p_db, p_sql, p_params)
 	v_cur.close()
 	return 0
